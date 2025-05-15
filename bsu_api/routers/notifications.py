b9052c63f28499f089024bce51f2305710c9e38b
EnteryName <ferrari.lorenzo04@gmail.com>
2025-05-15 23:18:59 +0200

    implement lock mechanism for clients

--- bsu_api/routers/notifications.py
+++ bsu_api/routers/notifications.py
@@ -6,6 +6,7 @@ from typing import Annotated
 
 
 router = APIRouter(prefix="/notification", tags=["Fake Data"])
+clients_lock = asyncio.Lock()
 clients: dict[str, asyncio.Queue] = {}
 
 
@@ -15,7 +16,9 @@ async def read_robot_notification(file: Annotated[bytes, File()]):
     Receive information about a robot
     """
     content = file.decode()
-    for client_id, queue in clients.items():
+    async with clients_lock:
+        queues = list(clients.values())
+    for queue in queues:
         await queue.put(content)
     return {"message": "Robot notification received"}
 
@@ -25,7 +28,9 @@ async def send_clients_notification(message: Annotated[str, Body(embed=True)]):
     """
     Send message to all the clients
     """
-    for client_id, queue in clients.items():
+    async with clients_lock:
+        queues = list(clients.values())
+    for queue in queues:
         await queue.put(message)
     return {"message": "Message sent to all clients"}
 
@@ -37,7 +42,8 @@ async def send_data():
     """
     client_id = str(uuid.uuid4())
     queue = asyncio.Queue()
-    clients[client_id] = queue
+    async with clients_lock:
+        clients[client_id] = queue
 
     async def event_generator():
         try:
@@ -48,6 +54,7 @@ async def send_data():
                 yield f"{sse_data}\n"
                 queue.task_done()
         finally:
-            clients.pop(client_id, None)
+            async with clients_lock:
+                clients.pop(client_id, None)
 
     return StreamingResponse(event_generator(), media_type="text/event-stream")
