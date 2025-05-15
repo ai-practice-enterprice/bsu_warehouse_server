e9a751839608674055d1d3fa14e680c756df1de8
EnteryName <ferrari.lorenzo04@gmail.com>
2025-05-15 15:15:16 +0200

    implement notification for every robot as an api endpoint

--- bsu_api/routers/notifications.py
+++ bsu_api/routers/notifications.py
@@ -1,9 +1,8 @@
 import asyncio
 import uuid
-from fastapi import APIRouter, File
+from fastapi import APIRouter, File, Body
 from fastapi.responses import StreamingResponse
 from typing import Annotated
-from arq_worker.emailTemplates import JinjaEmailTemplateBuilder, EmailType
 
 
 router = APIRouter(prefix="/notification", tags=["Fake Data"])
@@ -19,6 +18,16 @@ async def read_robot_notification(file: Annotated[bytes, File()]):
     for client_id, queue in clients.items():
         await queue.put(content)
     return {"message": "Robot notification received"}
+
+
+@router.post("/all")
+async def send_clients_notification(message: Annotated[str, Body(embed=True)]):
+    """
+    Send message to all the clients
+    """
+    for client_id, queue in clients.items():
+        await queue.put(message)
+    return {"message": "Message sent to all clients"}
 
 
 @router.get("/sse")
